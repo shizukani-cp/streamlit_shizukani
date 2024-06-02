e150cabdbd5e496eb10a59e9f3f59fd9e8785003
takeaship <spiral.spirit.spider@gmail.com>
2024-06-02 22:34:26 +0900

    refactor フォーマッタを実行し、linterのエラーを修正

--- src/streamlit_shizukani/modules/__init__.py
+++ src/streamlit_shizukani/modules/__init__.py
@@ -1,14 +1,21 @@
+import datetime
+import multiprocessing
+import random
 import re
-from janome.tokenizer import Tokenizer
+import threading
+from typing import Callable
+
+import keyboard
 import requests
 from bs4 import BeautifulSoup
 from googletrans import Translator
-import random, datetime, multiprocessing, keyboard, threading
+from janome.tokenizer import Tokenizer
+
 from . import fight
 
 trans = Translator()
 
-week = ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日']
+week = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
 
 process = None
 shm = None
@@ -16,16 +23,18 @@ fight_process = None
 
 tokenizer = Tokenizer()
 
+
 class Finish:
     def __init__(self):
         pass
+
 
 class Fight:
     def __init__(self):
         self.speaks = []
         self.p = multiprocessing.Process(target=self.run)
         self.p.start()
-    
+
     def run(self):
         print("running run")
         while True:
@@ -34,22 +43,24 @@ class Fight:
             p = threading.Thread(target=fight.fight_voice_play)
             self.speaks.append(p)
             p.start()
-    
+
     def stop(self):
         self.p.terminate()
         for t in self.speaks:
             t.join()
 
+
 def greeting(*_):
     return "何だてめえ"
 
+
 def address(*_):
-    #print("call address")
+    # print("call address")
     r = requests.get("https://get.geojs.io/v1/ip/geo.json")
-    #print("geted request")
-    #print(r.text)
+    # print("geted request")
+    # print(r.text)
     data = r.json()
-    #print("generated json")
+    # print("generated json")
     """print(data['latitude'])
     print(data['longitude'])
     print(data['country'])
@@ -66,37 +77,44 @@ def address(*_):
     except KeyError:
         return error()
 
+
 def now(*_):
     n = datetime.datetime.now()
     return f"{n.year}年{n.month}月{n.day}日の{n.hour}時{n.minute}分だが何だ?"
+
 
 def today(*_):
     n = datetime.datetime.today()
     return f"{n.year}年{n.month}月{n.day}日だが何だ?"
 
+
 def day(*_):
     n = datetime.datetime.today()
     return f"{week[n.weekday()]}だが何だ？"
 
+
 def meaning(ch, _):
-    #print("called meaning")
+    # print("called meaning")
     try:
         res = requests.get(f"https://kotobank.jp/word/{ch[:-2]}").text
         soup = BeautifulSoup(res, "html.parser")
-        description = soup.find('section', class_='description')
-        #print(type(description), str(description))
+        description = soup.find("section", class_="description")
+        # print(type(description), str(description))
         tag_re = """<("[^"]*"|'[^']*'|[^'">])*>"""
-        mea = re.sub(tag_re, '', str(description).replace(' ', ''))
-        if mea == None: return "何も見つかんなかったぞくそが"
+        mea = re.sub(tag_re, "", str(description).replace(" ", ""))
+        if mea is None:
+            return "何も見つかんなかったぞくそが"
         return f"{mea}だって。※コトバンク情報"
     except Exception as e:
         print(e)
         return error()
 
+
 def fight_start(*_):
     global fight_process
     fight_process = Fight()
     return "よっしゃケンカしたる"
+
 
 def fight_finish(*_):
     if fight_process is None:
@@ -104,14 +122,17 @@ def fight_finish(*_):
     fight_process.stop()
     return "ケンカ終わりにするでー"
 
+
 def finish(*_):
-    #print("called finish")
+    # print("called finish")
     return Finish
+
 
 def error():
     return random.choice(["あんだって？", "やべえエラー出た。"])
 
-rq_fun = [
+
+rq_fun: list[tuple[str, Callable]] = [
     ("オワリニシテ", finish),
     ("ネエシズカニ", greeting),
     ("ココドコ", address),
@@ -123,10 +144,12 @@ rq_fun = [
     (".+トハ", meaning),
 ]
 
-def main(rq:str) -> str:
+
+def main(rq: str) -> str:
     try:
         req = ""
-        for token in tokenizer.tokenize(rq): req += token.reading
+        for token in tokenizer.tokenize(rq):
+            req += token.reading
         req = req.replace("*", "")
         print(req)
         for word, func in rq_fun:
@@ -134,9 +157,10 @@ def main(rq:str) -> str:
                 return func(rq, req)
         print("not key")
         return error()
-        #return rq_fun[m]()
-    except:
+        # return rq_fun[m]()
+    except Exception:
         return error()
+
 
 if __name__ == "__main__":
     print(main("今日は何の日?"))
