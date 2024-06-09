8a8804d26edf98310b53cbdeb1fc01fede7bebba
shizukani-cp <to.maimai0606@gmail.com>
2024-06-09 14:58:39 +0900

    GUIを実装

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+from src.streamlit_shizukani.modules.main import main as responce
 
-from src.streamlit_shizukani.modules.main import main
+if "history" not in tuple(st.session_state):
+    st.session_state["history"] = ""
 
-main("ここどこ")
-st.text("this is tmp")
+with st.form("my_form", clear_on_submit=True):
+    input_text = st.text_input("静カニに話したいことは？")
+    submitted = st.form_submit_button("話す！")
+    if submitted:
+        st.session_state["history"] += f"あなた: {input_text}\n"
+        with st.spinner("考え中…"):
+            responce_text = responce(input_text)
+            breaked_text = ""
+            for i in range(0, len(responce_text), 48):
+                breaked_text += f"{responce_text[i:i+48]}\n"
+        st.session_state["history"] += f"静カニ: {breaked_text}\n"
+    text = st.text(st.session_state["history"])
